8616c833588c8b1693e013f170af0a88f4421dc9
ragbx <ragbx@ovh.fr>
2016-10-05 22:47:19 +0200

    MaJ stat_web

--- scripts/es_web.pl
+++ scripts/es_web.pl
@@ -1,47 +1,69 @@
 #!/usr/bin/perl
 
-use strict;
-use warnings ;
-use JSON ;
-use WWW::Mechanize;
-use HTTP::Cookies ;
+#use strict ;
+#use warnings ;
+use utf8 ;
+use DBI ;
 use DateTime ;
 use DateTime::Format::MySQL ;
-use FindBin qw( $Bin ) ;
+use Search::Elasticsearch ; 
+use FindBin qw( $Bin );
 use YAML qw(LoadFile) ;
-use Data::Dumper ;
 
 use lib "$Bin/modules/" ;
+use fonctions ;
 use dbrequest ;
+use esrbx ;
 
-# On récupère les infos de connexion à Piwik
-my $fic_conf = "$Bin/../conf.yaml" ;
-my $conf = LoadFile($fic_conf);
-my $piwik_url_api = $conf->{piwik}->{url_api} ;
-my $piwik_token_auth = $conf->{piwik}->{token_auth} ;
+# On récupère l'adresse d'Elasticsearch
+my $es_node = es_node() ;
+my $index = "web" ;
+my @types = qw( site bn-r ) ;
 
-my $portail_veille_sessions = portail_veille_sessions($piwik_url_api, $piwik_token_auth) ;
-print Dumper($portail_veille_sessions) ;
-
-sub portail_veille_sessions {
-	my ($piwik_url_api, $piwik_token_auth) = @_ ;
-	my $method = "VisitsSummary.getVisits" ;
-	my $idSite = "4" ;
-	my $period = "range" ;
-	my $date = "2015-09-08,2016-09-12" ;
-	my $format = "json" ;
-	my $url_stat = "$piwik_url_api&method=$method&idSite=$idSite&period=$period&date=$date&format=$format&token_auth=$piwik_token_auth" ;
+for my $type (@types) {
+	my $es_maxdatetime = es_maxdatetime("web", $type, "date") ;
+	print "$type : $es_maxdatetime\n" ;
+	web( $es_maxdatetime, $es_node, $index, $type ) ;
+}
 	
-	my $mech = WWW::Mechanize->new(
-		agent      => => 'MedRbx',
-		cookie_jar => HTTP::Cookies->new( autosave       => 1 )
-	) ;
-	
-	$mech->get($url_stat);
-	my $result = $mech->response()->decoded_content() ;
-	$result = decode_json $result ;
-	my %result = %$result ;
-	#my $value = $result{'value'} ;
-	#return $value ;
-	return $result ;
+sub web {
+	my ( $date, $es_node, $index, $type ) = @_ ;
+	my %params = ( nodes => $es_node ) ;
+
+	my $e = Search::Elasticsearch->new( %params ) ;
+
+	my $bdd = "statdb" ;
+	my $dbh = dbh($bdd) ;
+	my $req = <<SQL;
+SELECT
+	date,
+	site,
+	nb_sessions,
+	nb_pages_vues
+FROM statdb.stat_web
+WHERE site = ? AND date > ?
+SQL
+
+	my $sth = $dbh->prepare($req);
+	$sth->execute($type, $date);
+	while (my @row = $sth->fetchrow_array) {
+		my ( $date, $site, $nb_sessions, $nb_pages_vues ) = @row ;
+		
+		my %index = (
+			index   => $index,
+			type    => $type,
+			body    => {
+				date => $date,
+				site => $site,
+				nb_sessions => $nb_sessions,
+				nb_pages_vues => $nb_pages_vues
+			}
+		) ;
+
+		$e->index(%index) ;
+
+		print "$date - $site\n" ;	
+	}
+	$sth->finish();
+	$dbh->disconnect();
 }
---
+++ scripts/web.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+perl statdb_web.pl ;
+perl es_web.pl ;
