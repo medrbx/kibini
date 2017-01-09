e48cc52ec205d9cbd748d1176df5e8e69ccf3fd5
ragbx <ragbx@ovh.fr>
2017-01-09 15:08:44 +0100

    Mise en place extraction mappings ES

--- lib/kibini/elasticsearch.pm
+++ lib/kibini/elasticsearch.pm
@@ -2,10 +2,12 @@ package kibini::elasticsearch ;
 
 use Exporter ;
 @ISA = qw( Exporter ) ;
-@EXPORT = qw( GetEsNode ) ;
+@EXPORT = qw( GetEsNode RegenerateIndex ) ;
 
 use strict ;
 use warnings ;
+use YAML qw(LoadFile) ;
+use Search::Elasticsearch ; 
 
 use kibini::config ;
 
@@ -14,6 +16,36 @@ sub GetEsNode {
     my $node = $conf_elasticsearch->{node} ;
 
     return $node ;
+}
+
+sub GetEsMappingConf {
+    my ( $mapping_name ) = @_ ;
+
+    my $conf_elasticsearch = GetConfig('elasticsearch') ;
+    my $mappings_file = $conf_elasticsearch->{mappings_file} ;
+	my $mappings_data = LoadFile($mappings_file) ;
+    my $mappings = $mappings_data->{$mapping_name} ;
+	
+	return $mappings ;
+}
+
+sub RegenerateIndex {
+    my ( $node, $index_name ) = @_ ;
+
+    my %params = ( nodes => $node ) ;
+    my $mappings = GetEsMappingConf($index_name) ;
+    my %index_conf = (
+        index => $index_name,
+        body => $mappings
+    ) ;
+    
+    my $index_conf = \%index_conf ;
+    
+    my $e = Search::Elasticsearch->new( %params ) ;
+    $e->indices->delete( index => $index_name );
+    my $result = $e->indices->create( %index_conf );
+    
+    return $result ;
 }
 
 1;
