18d4860edd522bde822cd80be47393fb9523e6f8
ragbx <ragbx@ovh.fr>
2017-01-09 15:17:23 +0100

    Mise en place extraction es mappings

--- bin/admin_es_mappings.sh
+++ bin/admin_es_mappings.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-rm /home/kibini/kibini_dev/etc/es_mappings.yaml
+rm /home/kibini/kibini_prod/etc/es_mappings.yaml
 
 for index in adherents adherents2015 adherents_synth adherents_tout catalogue documents_synth eliminations entrees freq_etude frequentation inscrits_synth_carte items reservations rfid syntheses titres web webkiosk
 
 do
-    curl -XGET localhost:9200/$index/_mapping > /home/kibini/kibini_dev/etc/map.json
-    catmandu convert JSON to YAML < /home/kibini/kibini_dev/etc/map.json >> /home/kibini/kibini_dev/etc/es_mappings.yaml
+    curl -XGET localhost:9200/$index/_mapping > /home/kibini/kibini_prod/etc/map.json
+    catmandu convert JSON to YAML < /home/kibini/kibini_prod/etc/map.json >> /home/kibini/kibini_prod/etc/es_mappings.yaml
 done
 
-rm /home/kibini/kibini_dev/etc/map.json
+rm /home/kibini/kibini_prod/etc/map.json
