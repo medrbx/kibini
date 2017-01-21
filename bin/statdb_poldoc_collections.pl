c032a884ea3bb1d4891dadd2b1ff5556ed4de671
ragbx <ragbx@ovh.fr>
2017-01-21 11:11:01 +0100

    Début travail sur page collection

---
+++ bin/statdb_poldoc_collections.pl
@@ -0,0 +1,58 @@
+#! /usr/bin/perl
+
+use strict ;
+use warnings ;
+use utf8 ;
+use FindBin qw( $Bin ) ;
+
+use lib "$Bin/../lib" ;
+use kibini::db ;
+use kibini::log ;
+use kibini::time ;
+use collections::categories ;
+
+my $log_message ;
+my $process = "statdb_poldoc_collections.pl" ;
+# On log le début de l'opération
+$log_message = "$process : beginning" ;
+#AddCrontabLog($log_message) ;
+
+my $dbh = GetDbh() ;
+
+my @sites = qw( GP MD ZB CL ) ;
+my $site ;
+
+my %siteClauseWhere = (
+	"GP" => "i.location != 'ZZ'",
+	"MD" => "i.location NOT IN('MED0A', 'BUS1A')",
+	"ZB" => "i.location = 'BUS1A'",
+	"CL" => "i.location = 'MED0A'"
+) ;
+
+foreach $site (@sites) {
+	my $req = <<SQL;
+INSERT INTO statdb.stat_poldoc_collections(date, site, ccode, support, location, nb_exemplaires, nb_exemplaires_empruntables)
+SELECT
+	CURDATE(),
+	"$site",
+	i.ccode,
+	bi.itemtype,
+	i.location,
+	COUNT(i.itemnumber),
+	COUNT(IF(i.notforloan = 0, i.itemnumber, NULL))
+FROM koha_prod.items i
+JOIN koha_prod.biblioitems bi ON bi.biblionumber = i.biblionumber
+WHERE i.location != 'MUS1A'
+	AND i.notforloan != 4
+	AND $siteClauseWhere{$site}
+GROUP BY i.ccode, bi.itemtype, i.location
+SQL
+	my $sth = $dbh->prepare($req);
+	$sth->execute();
+}
+
+
+
+# On log la fin de l'opération
+$log_message = "$process : ending\n" ;
+#AddCrontabLog($log_message) ;
