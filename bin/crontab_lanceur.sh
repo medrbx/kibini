5d833943b03522dd7bb0ae4031d2c413558b939a
ragbx <ragbx@ovh.fr>
2017-02-24 18:14:37 +0100

    Remplacement tabs par espaces

--- bin/crontab_lanceur.sh
+++ bin/crontab_lanceur.sh
@@ -9,22 +9,22 @@ dir_log='/home/kibini/kibini_prod/log/crontab/'
 # CHAQUE MERCREDI
 if [ $dayofweek -eq 3 ]
 then
-	# On anonymise koha_prod, puis on réalise un dump de koha_prod et de statdb
-	perl $dir/admin_sauv_bdd.pl
-	# On met à jour les entrées, webkiosk, les adhérents dans ES
-	perl $dir/statdb_borrowers.pl
-	perl $dir/es_entrees.pl
-	perl $dir/es_webkiosk.pl
-	perl $dir/es_borrowers.pl
-	# On met à jour la carte des quartiers
-	perl $dir/data_carte.pl
+    # On anonymise koha_prod, puis on réalise un dump de koha_prod et de statdb
+    perl $dir/admin_sauv_bdd.pl
+    # On met à jour les entrées, webkiosk, les adhérents dans ES
+    perl $dir/statdb_borrowers.pl
+    perl $dir/es_entrees.pl
+    perl $dir/es_webkiosk.pl
+    perl $dir/es_borrowers.pl
+    # On met à jour la carte des quartiers
+    perl $dir/data_carte.pl
 fi
 
 # CHAQUE PREMIER MERCREDI DU MOIS
 if [ $dayofweek -eq 3 ] && [ $dayofmonth -lt 8 ]
 then
-	# On fait un cliché des données adhérents
-	perl $dir/es_borrowers_synth.pl
+    # On fait un cliché des données adhérents
+    perl $dir/es_borrowers_synth.pl
 fi
 
 # CHAQUE JOUR
@@ -56,16 +56,16 @@ perl $dir/logs_portail.pl
 # CHAQUE MARDI
 if [ $dayofweek -eq 2 ]
 then
-	# On incorpore dans statdb des données sur les exemplaires et les adhérents
-	perl $dir/statdb_items_borrowers.pl
+    # On incorpore dans statdb des données sur les exemplaires et les adhérents
+    perl $dir/statdb_items_borrowers.pl
 fi
 
 # CHAQUE DIMANCHE
 if [ $dayofweek -eq 7 ]
 then
-	# On recrée les index items et catalogue dans ES
-	perl $dir/es_items.pl
-	bash $dir/catmandu_es.sh
+    # On recrée les index items et catalogue dans ES
+    perl $dir/es_items.pl
+    bash $dir/catmandu_es.sh
 
 fi
 
