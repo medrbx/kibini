4b4be17c63f5311a72bac798a7267239c76181c9
ragbx <ragbx@ovh.fr>
2019-02-01 15:54:15 +0100

    Améliorations lib/Adherents (2)

--- lib/Adherent.pm
+++ lib/Adherent.pm
@@ -82,20 +82,35 @@ has koha_checkprevcheckout => ( is => 'ro' );
 has koha_updated_on => ( is => 'ro' );
 has koha_lastseen => ( is => 'ro' );
 
+has statdb_sexe => ( is => 'ro' );
 has statdb_age => ( is => 'ro' );
-has statdb_acode => ( is => 'ro' );
+has statdb_age_code => ( is => 'ro' );
 has statdb_ville => ( is => 'ro' );
-has statdb_iris => ( is => 'ro' );
+has statdb_rbx_iris => ( is => 'ro' );
 has statdb_categorycode => ( is => 'ro' );
 has statdb_branchcode => ( is => 'ro' );
-has statdb_fidelite => ( is => 'ro' );
-has statdb_sexe => ( is => 'ro' );
+has statdb_nb_annees_adhesion => ( is => 'ro' );
+has statdb_userid => ( is => 'ro' );
+has statdb_borrowernumber => ( is => 'ro' );
 has statdb_adherentid => ( is => 'ro' );
 
 has es_sexe => ( is => 'ro' );
+has es_age => ( is => 'ro' );
 has es_age_lib1 => ( is => 'ro' );
 has es_age_lib2 => ( is => 'ro' );
 has es_age_lib3 => ( is => 'ro' );
+has es_ville => ( is => 'ro' );
+has es_rbx_iris => ( is => 'ro' );
+has es_rbx_nom_iris => ( is => 'ro' );
+has es_rbx_quartier => ( is => 'ro' );    
+has es_rbx_secteur => ( is => 'ro' );
+has es_carte => ( is => 'ro' );
+has es_type_carte => ( is => 'ro' );
+has es_personnalite  => ( is => 'ro' );
+has es_site_inscription => ( is => 'ro' );    
+has es_nb_annees_adhesion => ( is => 'ro' );
+has es_adherentid => ( is => 'ro' );
+
 
 sub BUILDARGS {
     my ($class, @args) = @_;
@@ -147,7 +162,7 @@ sub get_data_from_koha_by_id {
     return $self;
 }
 
-sub get_age_at_time_of_event {
+sub get_statdb_age {
     my ($self, $param) = @_;
     
     my $date_event = $self->{$param->{date_event_field}};
@@ -168,7 +183,163 @@ sub get_age_at_time_of_event {
     return $self;
 }
 
-sub get_age_data {
+# TO DO
+sub get_statdb_age_code {
+    my ($self) = @_;
+     
+    return $self;
+}
+
+sub get_statdb_ville {
+    my ($self) = @_;
+    
+    $self->{statdb_ville} = $self->{koha_city};
+    
+    return $self;
+}
+
+sub get_statdb_rbx_iris {
+    my ($self) = @_;
+    
+    $self->{statdb_iris} = $self->{koha_altcontactcountry};
+    
+    return $self;
+}
+
+sub get_statdb_branchcode {
+    my ($self) = @_;
+    
+    $self->{statdb_ville} = $self->{koha_branchcode};
+    
+    return $self;
+}
+
+sub get_statdb_categorycode {
+    my ($self) = @_;
+    
+    $self->{statdb_categorycode} = $self->{koha_categorycode};
+    
+    return $self;
+}
+
+sub get_statdb_nb_annees_adhesion {
+    my ($self, $param) = @_;
+    
+    if ($self->{koha_dateenrolled}) {
+        my $date_event = $self->{$param->{date_event_field}};
+    
+        my $yearofevent;
+        if ( $param->{format_date_event} eq 'datetime' ) {
+            $yearofevent = DateTime::Format::MySQL->parse_datetime($date_event)->year();
+        } elsif ( $param->{format_date_event} eq 'date' ) {
+            $yearofevent = DateTime::Format::MySQL->parse_date($date_event)->year();
+        }
+
+        my $yearenrolled = DateTime::Format::MySQL->parse_date($self->{koha_dateenrolled})->year();
+    
+        $self->{statdb_nb_annees_adhesion} = $yearofevent - $yearenrolled;
+    }
+    
+    return $self;
+}
+
+sub get_statdb_sexe {
+    my ($self) = @_;
+    
+    my @categorycodes = qw( MEDA MEDB MEDC CSVT MEDP BIBL CSLT );
+    my $categorycode;
+    if ($self->{koha_categorycode} ) {
+        $categorycode = $self->{koha_categorycode};
+    } elsif ($self->{statdb_categorycode} ) {
+        $categorycode = $self->{statdb_categorycode};
+    }
+    
+    if ( any { /$categorycode/ } @categorycodes ) {
+        if ( $self->{koha_title} ) {
+            if ( $self->{koha_title} eq 'Madame' ) {
+                $self->{statdb_sexe} = 'F';
+            } elsif ( $self->{koha_title} eq 'Monsieur' ) {
+                $self->{statdb_sexe} = 'M';
+            } else {
+                $self->{statdb_sexe} = 'NC';
+            }
+        }
+    } else {
+        $self->{statdb_sexe} = 'NP';
+    }
+    
+    return $self;
+}
+
+sub get_statdb_userid {
+    my ($self) = @_;
+    
+    $self->{statdb_userid} = $self->{koha_userid};
+    
+    return $self;
+}
+
+sub get_statdb_borrowernumber {
+    my ($self) = @_;
+    
+    $self->{statdb_borrowernumber} = $self->{koha_borrowernumber};
+    
+    return $self;
+}
+
+sub get_statdb_adherentid {
+    my ($self) = @_;
+    
+    my $crypter = $self->{crypter};
+    
+    $self->{statdb_adherentid} = $crypter->crypt($self->{koha_borrowernumber});
+    
+    return $self;
+}
+
+sub get_es_sexe {
+    my ($self) = @_;
+    
+    if ( $self->{statdb_sexe} ) {
+        if ($self->{statdb_sexe} eq 'F') {
+            $self->{es_sexe} = 'Femme';
+        } elsif ($self->{statdb_sexe} eq 'M') {
+            $self->{es_sexe} = 'Homme';
+        } elsif ($self->{statdb_sexe} eq 'NC') {
+            $self->{es_sexe} = 'Inconnu';
+        } elsif ($self->{statdb_sexe} eq 'M') {
+            $self->{es_sexe} = 'Homme';
+        } elsif ($self->{statdb_sexe} eq 'NP') {
+            $self->{es_sexe} = 'NP';
+        }
+    } else {    
+        my @categorycodes = qw( MEDA MEDB MEDC CSVT MEDP BIBL CSLT );
+        my $categorycode;
+        if ($self->{koha_categorycode} ) {
+            $categorycode = $self->{koha_categorycode};
+        } elsif ($self->{statdb_categorycode} ) {
+            $categorycode = $self->{statdb_categorycode};
+        }
+    
+        if ( any { /$categorycode/ } @categorycodes ) {
+            if ( $self->{koha_title} ) {
+                if ( $self->{koha_title} eq 'Madame' ) {
+                    $self->{es_sexe} = 'Femme';
+                } elsif ( $self->{koha_title} eq 'Monsieur' ) {
+                    $self->{es_sexe} = 'Homme';
+                } else {
+                    $self->{es_sexe} = 'Inconnu';
+                }
+            }
+        } else {
+            $self->{es_sexe} = 'NP';
+        }
+    }
+    
+    return $self;
+}
+
+sub get_es_age_labels {
     my ($self, $type) = @_;
     
     if ( $self->{statdb_acode} ) {
@@ -188,73 +359,83 @@ sub get_age_data {
     return $self;
 }
 
-sub get_fidelite {
-    my ($self, $param) = @_;
+sub get_es_ville {
+    my ($self) = @_;
     
-    if ($self->{koha_dateenrolled}) {
-        my $date_event = $self->{$param->{date_event_field}};
-    
-        my $yearofevent;
-        if ( $param->{format_date_event} eq 'datetime' ) {
-            $yearofevent = DateTime::Format::MySQL->parse_datetime($date_event)->year();
-        } elsif ( $param->{format_date_event} eq 'date' ) {
-            $yearofevent = DateTime::Format::MySQL->parse_date($date_event)->year();
-        }
-
-        my $yearenrolled = DateTime::Format::MySQL->parse_date($self->{koha_dateenrolled})->year();
-    
-        $self->{statdb_fidelite} = $yearofevent - $yearenrolled;
+    if ($self->{koha_city}) {
+        $self->{es_ville} = $self->{koha_city};
+    } else { 
+        $self->{es_ville} = $self->{statdb_ville};    
     }
     
     return $self;
 }
 
-sub get_sex {
+sub get_es_rbx_iris {
     my ($self) = @_;
     
-    my @categorycodes = qw( MEDA MEDB MEDC CSVT MEDP BIBL CSLT );
-    my $categorycode;
-    if ($self->{koha_categorycode} ) {
-        $categorycode = $self->{koha_categorycode};
-    } elsif ($self->{statdb_categorycode} ) {
-        $categorycode = $self->{statdb_categorycode};
-    }
-    
-    if ( any { /$categorycode/ } @categorycodes ) {
-        if ( $self->{koha_title} ) {
-            if ( $self->{koha_title} eq 'Madame' ) {
-                $self->{statdb_sexe} = 'F';
-                $self->{es_sexe} = 'Femme';
-            } elsif ( $self->{koha_title} eq 'Monsieur' ) {
-                $self->{statdb_sexe} = 'M';
-                $self->{es_sexe} = 'Homme';
-            } else {
-                $self->{statdb_sexe} = 'NC';
-                $self->{es_sexe} = 'Inconnu';
-            }
-        } elsif ( $self->{statdb_sexe} ) {
-            if ( $self->{statdb_sexe} eq 'F' ) {
-                $self->{es_sexe} = 'Femme';
-            } elsif ( $self->{statdb_sexe} eq 'H' ) {
-                $self->{es_sexe} = 'Homme';
-            } else {
-                $self->{es_sexe} = 'Inconnu';
-            }
-        }
-    } else {
-        $self->{statdb_sexe} = 'NP';
-        $self->{es_sexe} = 'NP';
+    if ($self->{koha_city}) {
+        $self->{es_rbx_iris} = $self->{koha_altcontactcountry};
+    } else { 
+        $self->{es_rbx_iris} = $self->{statdb_iris};    
     }
     
     return $self;
 }
 
-sub get_adherentid {
+sub get_es_rbx_nom_iris {
     my ($self) = @_;
     
-    my $crypter = $self->{crypter};
+    return $self;
+}
+
+sub get_es_rbx_quartier {
+    my ($self) = @_;
     
-    $self->{statdb_adherentid} = $crypter->crypt($self->{koha_borrowernumber});
+    return $self;
+}
+
+sub get_es_rbx_secteur {
+    my ($self) = @_;
+    
+    return $self;
+}
+
+sub get_es_carte {
+    my ($self) = @_;
+    
+    return $self;
+}
+
+sub get_es_type_carte {
+    my ($self) = @_;
+    
+    return $self;
+}
+
+sub get_es_personanalite {
+    my ($self) = @_;
+    
+    return $self;
+}
+
+sub get_es_nb_annees_adhesion {
+    my ($self) = @_;
+    
+    $self->{es_nb_annees_adhesion} = $self->{statdb_nb_annees_adhesion};
+    
+    return $self;
+}
+
+sub get_es_adherentid {
+    my ($self) = @_;
+    
+    if ($self->{statdb_adherentid}) {
+        $self->{es_adherentid} = $self->{statdb_adherentid};
+    } elsif ($self->{koha_borrowernumber}) {    
+        my $crypter = $self->{crypter};
+        $self->{statdb_adherentid} = $crypter->crypt($self->{koha_borrowernumber});
+    }
     
     return $self;
 }
