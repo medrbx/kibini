9411f0250db3bec1fd2c3f3b1293ad7413a1feff
ragbx <ragbx@ovh.fr>
2019-02-02 10:12:17 +0100

    Amélioration Adherent (3)

--- bin/data_webkiosk.pl
+++ bin/data_webkiosk.pl
@@ -28,13 +28,13 @@ my $csv = Text::CSV->new ({
     binary    => 1, # permet caractères spéciaux (?)
     auto_diag => 1, # permet diagnostic immédiat des erreurs
 });
-$csv->column_names (qw( wk_heure_deb wk_heure_fin wk_espace wk_poste koha_userid ));
+$csv->column_names (qw( session_heure_deb session_heure_fin session_espace session_poste koha_userid ));
 
 while ( my $row = $csv->getline_hr ($fic) ) {
     my $wk = Webkiosk->new( { dbh => $dbh, crypter => $crypter, wk => $row } );
-    $wk->get_wkuser_from_koha;
-    $wk->get_wkuser_data;
-   # $wk->add_data_to_statdb_webkiosk;
+	$wk->get_wkuser_from_koha;
+	$wk->get_wkuser_data;
+#	$wk->add_data_to_statdb_webkiosk;
 	
     
    print Dumper($wk);
