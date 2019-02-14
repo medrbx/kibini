b8ab298c6311cce4629910d0e0505c4e42b9edb7
ragbx <ragbx@ovh.fr>
2019-02-14 14:32:56 +0100

    Logs

--- bin/data_webkiosk.pl
+++ bin/data_webkiosk.pl
@@ -8,14 +8,15 @@ use FindBin qw( $Bin );
 use lib "$Bin/../lib";
 use Kibini::DB;
 use Kibini::Crypt;
-use kibini::log;
+use Kibini::Log;
 use Webkiosk;
 
+my $log = Kibini::Log->new;
 my $log_message;
 my $process = "statdb_webkiosk.pl";
 # On log le début de l'opération
 $log_message = "$process : beginning";
-AddCrontabLog($log_message);
+$log->add_log($log_message);
 
 my $dbh = Kibini::DB->new;
 $dbh = $dbh->dbh;
@@ -30,18 +31,20 @@ my $csv = Text::CSV->new ({
 });
 $csv->column_names (qw( session_heure_deb session_heure_fin session_groupe session_poste koha_userid ));
 
+my $i = 0;
 while ( my $row = $csv->getline_hr ($fic) ) {
     my $wk = Webkiosk->new( { dbh => $dbh, crypter => $crypter, wk => $row } );
     $wk->get_wkuser_from_koha;
     $wk->get_wkuser_data;
     $wk->add_data_to_statdb_webkiosk;
     $wk->add_data_to_es_webkiosk;
-    print Dumper($wk);
+#    print Dumper($wk);
+    $i++;
 }
 
 close $fic;
 $dbh->disconnect();
 
 # On log la fin de l'opération
-$log_message = "$process : ending\n";
-AddCrontabLog($log_message);
+$log->add_log("$process : $i rows added");
+$log->add_log("$process : ending\n");
