72c9e523e5f491b17a65fdf06bbfe924ad5caf8e
ragbx <ragbx@ovh.fr>
2019-02-14 12:10:53 +0100

    Ajout get_es_max_date_and_time

--- lib/Kibini/ES.pm
+++ lib/Kibini/ES.pm
@@ -7,13 +7,35 @@ use Kibini::Config;
 
 has e => ( is => 'ro', builder => '_get_e' );
 
+
+sub get_es_max_date_and_time {
+    my ($self, $param) = @_;
+        
+    my $e = $self->{e};    
+    my $result =  $e->search(
+        index => $param->{index},
+        type  => $param->{type},
+        body    => {
+            aggs       => {
+                max_datetime => {
+                    max => {
+                        field => $param->{field}
+                    }
+                }
+            }
+        }
+    );
+
+    return $result->{aggregations}->{max_datetime}->{value_as_string} ; 
+}
+
 sub _get_e {
     my $conf_elasticsearch = Kibini::Config->new->elasticsearch;
     my $es_node = $conf_elasticsearch->{node};
     my %params = ( nodes => $es_node );
 
     my $e = Search::Elasticsearch->new( nodes => $es_node );
-	
+    
     return $e;
 }
 
