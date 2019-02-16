2e56c16b8159bdbe72ccb3d5fda839b6b50cdee1
ragbx <ragbx@ovh.fr>
2019-02-16 14:58:01 +0100

    Mise en place Evenement

--- bin/data_webkiosk.pl
+++ bin/data_webkiosk.pl
@@ -21,22 +21,24 @@ $dbh = $dbh->dbh;
 
 my $crypter = Kibini::Crypt->new;
 
-open my $fic, "<", "/home/kibini/wk_users_logs_consommations.csv";
+open my $fic, "<", "/home/fpichenot/Documents/projets/data/wbk_semaine_5.csv"; #"/home/kibini/wk_users_logs_consommations.csv";
 
 my $csv = Text::CSV->new ({
     binary    => 1, # permet caractères spéciaux (?)
     auto_diag => 1, # permet diagnostic immédiat des erreurs
 });
-$csv->column_names (qw( session_heure_deb session_heure_fin session_groupe session_poste koha_userid ));
+$csv->column_names (qw( date_heure_a date_heure_b session_groupe session_poste koha_userid ));
 
 my $i = 0;
 while ( my $row = $csv->getline_hr ($fic) ) {
+    
     my $wk = Webkiosk->new( { dbh => $dbh, crypter => $crypter, wk => $row } );
+    $wk->evenement_complete_data({date_heure_a_format => 'datetime', date_heure_b_format => 'datetime'});
     $wk->get_wkuser_from_koha;
     $wk->get_wkuser_data;
-    $wk->add_data_to_statdb_webkiosk;
-    $wk->add_data_to_es_webkiosk;
-#    print Dumper($wk);
+#    $wk->add_data_to_statdb_webkiosk;
+#    $wk->add_data_to_es_webkiosk;
+    print Dumper($wk);
     $i++;
 }
 
