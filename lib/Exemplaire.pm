9fb92dd2c857f79194932d8ca2d7972db208fe4b
ragbx <ragbx@ovh.fr>
2019-02-21 17:32:16 +0100

    Mise en place Document

--- lib/Exemplaire.pm
+++ lib/Exemplaire.pm
@@ -5,14 +5,7 @@ use utf8;
 
 with 'Document';
 
-sub get_exemplaire_from_koha {
-    my ($self) = @_;
 
-    my @koha_fields = ("i.biblionumber","i.barcode","b.title");
-    $self->get_document_data_from_koha_by_id( { koha_fields => \@koha_fields, koha_id => 'itemnumber' } );
-    
-    return $self;
-}
 
 1;
 
