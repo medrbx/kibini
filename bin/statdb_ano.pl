e6b07f4a5127416a40c7832cb450571cc07aaf18
ragbx <ragbx@ovh.fr>
2019-02-22 10:01:04 +0100

    Mise en place ano

---
+++ bin/statdb_ano.pl
@@ -0,0 +1,21 @@
+#! /usr/bin/perl
+
+use Modern::Perl;
+use FindBin qw( $Bin );
+
+use lib "$Bin/../lib";
+use Kibini::Log;
+use Webkiosk;
+
+my $log = Kibini::Log->new;
+my $process = "statdb_ano.pl";
+$log->add_log("$process : beginning");
+
+my $i;
+
+# Webkiosk
+my $wk = Webkiosk->new;
+$i = $wk->ano_statdb_sessions_webkiosk;
+$log->add_log("statdb_sessions_webkiosk : $i rows anonymised");
+
+$log->add_log("$process : ending");
