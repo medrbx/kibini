ae384113523e8722e26e48edc0588ef876fee6e4
ragbx <ragbx@ovh.fr>
2019-05-04 18:19:21 +0200

    Ajout regenerate_index

--- lib/Kibini/ES.pm
+++ lib/Kibini/ES.pm
@@ -1,11 +1,14 @@
 package Kibini::ES;
 
 use Moo;
+use YAML qw(LoadFile);
 use Search::Elasticsearch;
 
 use Kibini::Config;
 
 has e => ( is => 'ro', builder => '_get_e' );
+has conf => ( is => 'ro', builder => '_get_conf' );
+has mappings => ( is => 'ro');
 
 
 sub get_es_max_date_and_time {
@@ -26,7 +29,32 @@ sub get_es_max_date_and_time {
         }
     );
 
-    return $result->{aggregations}->{max_datetime}->{value_as_string} ; 
+    return $result->{aggregations}->{max_datetime}->{value_as_string}; 
+}
+
+sub get_mappings {
+    my ( $self ) = @_;
+
+    my $mappings_file = $self->{conf}->{mappings_file};
+    $self->{mappings} = LoadFile($mappings_file);
+    
+    return $self;
+}
+
+sub regenerate_index {
+    my ( $self, $index_name ) = @_;
+
+    my $mapping = $self->{mappings}->{$index_name};
+    my %index_conf = (
+        index => $index_name,
+        body => $mapping
+    );
+
+    my $e = $self->{e};
+    $e->indices->delete( index => $index_name );
+    my $result = $e->indices->create( %index_conf );
+    
+    return $result;
 }
 
 sub _get_e {
@@ -37,6 +65,12 @@ sub _get_e {
     my $e = Search::Elasticsearch->new( nodes => $es_node );
     
     return $e;
+}
+
+sub _get_conf {
+    my $conf = Kibini::Config->new->elasticsearch;
+   
+    return $conf;
 }
 
 1;
