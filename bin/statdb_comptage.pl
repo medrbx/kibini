5a34582f449b84f955e39d11f259e90446d20b54
ragbx <ragbx@ovh.fr>
2019-06-13 16:12:38 +0200

    MaJ

--- bin/statdb_comptage.pl
+++ bin/statdb_comptage.pl
@@ -1,32 +1,48 @@
 #! /usr/bin/perl
 
-#use Modern::Perl;
+use Modern::Perl;
 use Data::Dumper;
 use FindBin qw( $Bin );
 
 use lib "$Bin/../lib";
 use Kibini::Config;
+use Kibini::DB;
+use Kibini::Time;
+
+my $dbh = Kibini::DB->new;
+$dbh = $dbh->dbh;
+my $req = "INSERT INTO statdb.stat_compteur_entrees (date_heure, nb_entrees) VALUES (?, ?)";
+my $sth = $dbh->prepare($req);
+
+my $time = Kibini::Time->new;
+my $date = $time->get_date_and_time('yesterday YYYYMMDD');
 
 my $conf = Kibini::Config->new->comptage;
 my $directory = $conf->{directory};
-my $date = "20181130";
 my $file = $directory . "/" . $date . ".csv";
 
-# 05/12/2018, 00:00:00, 0, 0, 0, 0
-open(my $fh, '<', $file);
-my $in = 0;
-my $out = 0;
-while (my $row = <$fh>) {
-    chomp $row;
-    if ( $row =~ m/^\d{2}\/\d{2}\/\d{4}/ ) {
-		my $count = {};
-		($count->{day}, $count->{month}, $count->{year}, $count->{hour}, $count->{min_in}, $count->{min_out}) = ( $row =~ m/^(\d{2})\/(\d{2})\/(\d{4}),\s(\d{2}:\d{2}:\d{2}),\s(\d+),\s(\d+)/ );
-		#if ( $count->{min_in} != 0 || $count->{min_out} != 0 ) {
-			$in = $in + $count->{min_in};
-            $out = $out + $count->{min_out};
-            $count->{presents} = $in - $out;
-		    print "$count->{hour}|$count->{presents}\n";
-		#}
+open(my $fh, '<:encoding(UTF-8)', $file);
+my $count = {};
+while (my $string = <$fh>) {
+    chomp $string;
+    if ( $string =~ m/\d{2}\/\d{2}\/\d{4}/ ) {
+        $string =~ s/\s+//g;
+        my $row = {};
+        ($row->{date}, $row->{heure}, $row->{entrees}, $row->{sorties} ) = split /,/, $string;
+        if ( $row->{entrees} != 0 ) {
+            my ($day, $month, $year) = split /\//, $row->{date};
+            my $hour = substr($row->{heure}, 0, 2);
+            my $datetime = $year . "-" . $month . "-" . $day . " " . $hour . ":00:00";
+            if ($count->{$datetime}) {
+                $count->{$datetime} = $count->{$datetime} + $row->{entrees};
+            } else {
+                $count->{$datetime} = $row->{entrees};
+            }
+        }
     }
 }
-close $fh;
+
+my @keys = sort keys %$count;
+foreach my $k (@keys) {
+    $sth->execute($k, $count->{$k});
+}
