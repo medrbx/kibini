ffd5beb73bdb30abbb30ad9c90742308e174adec
ragbx <ragbx@ovh.fr>
2019-06-13 16:09:53 +0200

    MaJ

--- bin/admin_es_mappings.sh
+++ bin/admin_es_mappings.sh
@@ -2,7 +2,7 @@
 
 rm /home/kibini/kibini_prod/etc/es_mappings.yaml
 
-for index in action_coop action_culturelle adherents adherents_synth adherents_tout adherents2 adherents2015 catalogue eliminations entrees freq_etude frequentation items periodiques prets reservations rfid synth_ann_exemplaires synth_ann_inscrits synth_ann_prets web web2 webkiosk
+for index in action_coop action_culturelle adherents adherents_synth adherents_tout adherents2 adherents2015 catalogue eliminations entrees freq_etude frequentation items periodiques prets reservations rfid synth_ann_exemplaires synth_ann_inscrits synth_ann_prets web web2 webkiosk magasin
 
 do
     curl -XGET localhost:9200/$index/_mapping > /home/kibini/kibini_prod/etc/map.json
