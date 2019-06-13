a53e98dc617473a0096039b12b9dd3d332a9a9f1
ragbx <ragbx@ovh.fr>
2019-06-13 16:14:47 +0200

    MaJ

--- bin/es_entrees.pl
+++ bin/es_entrees.pl
@@ -1,59 +1,58 @@
 #!/usr/bin/perl
 
-use strict ;
-use warnings ;
-use Search::Elasticsearch ; 
-use FindBin qw( $Bin ) ;
+use strict;
+use warnings;
+use Search::Elasticsearch; 
+use FindBin qw( $Bin );
+use Data::Dumper;
 
-use lib "$Bin/../lib" ;
-use kibini::db ;
-use kibini::elasticsearch ;
-use kibini::log ;
-use kibini::time ;
+use lib "$Bin/../lib";
+use kibini::db;
+use kibini::elasticsearch;
+use kibini::log;
+use kibini::time;
 
-my $log_message ;
-my $process = "es_entrees.pl" ;
+my $log_message;
+my $process = "es_entrees.pl";
 # On log le début de l'opération
-$log_message = "$process : beginning" ;
-AddCrontabLog($log_message) ;
+$log_message = "$process : beginning";
+AddCrontabLog($log_message);
 
 # On récupère l'adresse d'Elasticsearch
-my $es_node = GetEsNode() ;
+my $es_node = GetEsNode();
 
-my $es_maxdatetime = GetEsMaxDateTime("entrees", "camera", "entrees_date") ;
-my $i = entrees($es_maxdatetime, $es_node) ;
+my $es_maxdatetime = GetEsMaxDateTime("entrees", "camera", "entrees_date");
+my $i = entrees($es_maxdatetime, $es_node);
 
 # On log la fin de l'opération
-$log_message = "$process : $i rows indexed" ;
-AddCrontabLog($log_message) ;
-$log_message = "$process : ending\n" ;
-AddCrontabLog($log_message) ;
+$log_message = "$process : $i rows indexed";
+AddCrontabLog($log_message);
+$log_message = "$process : ending\n";
+AddCrontabLog($log_message);
 
 
 sub entrees {
-    my ($maxdatetime, $es_node) = @_ ;
-    my %params = ( nodes => $es_node ) ;
-    my $index = "entrees" ;
-    my $type = "camera" ;
+    my ($maxdatetime, $es_node) = @_;
+    my %params = ( nodes => $es_node );
+    my $index = "entrees";
+    my $type = "camera";
 
-    my $e = Search::Elasticsearch->new( %params ) ;
+    my $e = Search::Elasticsearch->new( %params );
 
-    my $dbh = GetDbh() ;
+    my $dbh = GetDbh();
     my $req = <<SQL;
-SELECT
-    datetime,
-    entrees
-FROM statdb.stat_entrees
-WHERE datetime > ?
+SELECT date_heure, nb_entrees
+FROM statdb.stat_compteur_entrees
+WHERE date_heure > ?
 SQL
 
     my $sth = $dbh->prepare($req);
     $sth->execute($maxdatetime);
-    $i = 0 ;
+    $i = 0;
     while (my @row = $sth->fetchrow_array) {
-        my ( $datetime, $entrees ) = @row ;
+        my ( $datetime, $entrees ) = @row;
 
-        my ($entrees_year, $entrees_month, $entrees_week_number, $entrees_day, $entrees_jour_semaine, $entrees_hour) = GetSplitDateTime($datetime) ;
+        my ($entrees_year, $entrees_month, $entrees_week_number, $entrees_day, $entrees_jour_semaine, $entrees_hour) = GetSplitDateTime($datetime);
     
         my %index = (
             index   => $index,
@@ -68,12 +67,13 @@ SQL
                 entrees_date_heure => $entrees_hour,
                 nb_entrees => $entrees
             }
-        ) ;
+        );
 
-        $e->index(%index) ;
-        $i++ ;
+        $e->index(%index);
+        #print Dumper(\%index);
+        $i++;
     }
     $sth->finish();
     $dbh->disconnect();
-    return $i ;
+    return $i;
 }
