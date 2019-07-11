eef3f675dc3d577b604eb86e63446200417786ce
ragbx <ragbx@ovh.fr>
2019-07-11 17:55:20 +0200

    MaJ

--- lib/action_culturelle.pm
+++ lib/action_culturelle.pm
@@ -1,28 +1,28 @@
-package action_culturelle ;
+package action_culturelle;
 
-use Exporter ;
-@ISA = qw( Exporter ) ;
-@EXPORT = qw( insert_action_culturelle list_actions AddEsLastAction_culturelle ) ;
+use Exporter;
+@ISA = qw( Exporter );
+@EXPORT = qw( insert_action_culturelle list_actions AddEsLastAction_culturelle );
 
-use strict ;
-use warnings ;
-use FindBin qw( $Bin ) ;
+use strict;
+use warnings;
+use FindBin qw( $Bin );
 
-use lib "$Bin/../lib" ;
-use kibini::db ;
-use kibini::elasticsearch ;
+use lib "$Bin/../lib";
+use kibini::db;
+use kibini::elasticsearch;
 
 sub AddEsLastAction_culturelle {
-    my $es_node = GetEsNode() ;
-    my %params = ( nodes => $es_node ) ;
-    my $index = "action_culturelle" ;
-    my $type = "actions" ;
-    my $max_id = GetEsMaxId($index, $type, 'action_id') ;
-    $max_id = 0 if ( !defined $max_id ) ;    
+    my $es_node = GetEsNode();
+    my %params = ( nodes => $es_node );
+    my $index = "action_culturelle";
+    my $type = "actions";
+    my $max_id = GetEsMaxId($index, $type, 'action_id');
+    $max_id = 0 if ( !defined $max_id );    
 
-    my $e = Search::Elasticsearch->new( %params ) ;
+    my $e = Search::Elasticsearch->new( %params );
 
-    my $dbh = GetDbh() ;
+    my $dbh = GetDbh();
     my $req = <<SQL;
 SELECT
     *
@@ -32,50 +32,51 @@ SQL
 
     my $sth = $dbh->prepare($req);
     $sth->execute($max_id);
-    my $i = 0 ;
+    my $i = 0;
     while (my @row = $sth->fetchrow_array) {
-        my ( $id, $date, $action, $lieu, $type_action, $public, $partenariat, $nb_participants ) = @row ;
-		
-		if (defined $date) {
+        my ( $id, $date, $action, $lieu, $type_action, $public, $partenariat, $nb_participants ) = @row;
+        
+        if (defined $date) {
     
-			my %index = (
-				index   => $index,
-				type    => $type,
-				id         => $id,
-				body    => {
-					action_id => $id,
-					date => $date,
-					action => $action,
-					lieu => $lieu,
-					type_action => $type_action,
-					public => $public,
-					partenariat => $partenariat,
-					nb_participants => $nb_participants
-				}
-			);
+            my %index = (
+                index   => $index,
+                type    => $type,
+                id         => $id,
+                body    => {
+                    action_id => $id,
+                    date => $date,
+                    action => $action,
+                    lieu => $lieu,
+                    type_action => $type_action,
+                    public => $public,
+                    partenariat => $partenariat,
+                    nb_participants => $nb_participants
+                }
+            );
+            $e->index(%index);
         }
 
-        $e->index(%index) ;
-        $i++ ;
+        
+        $i++;
     }
     $sth->finish();
     $dbh->disconnect();
-    return $i ;
+    return $i;
 }
 
 sub insert_action_culturelle {
-    my ( $date, $action, $lieu, $type, $public, $partenariat, $participants ) = @_ ;
-    my $dbh = GetDbh() ;
-    my $req = "INSERT INTO statdb.stat_action_culturelle (date, action, lieu, type, public, partenariat, participants) VALUES ( ?, ?, ?, ?, ?, ?, ?)" ;
+    my ( $date, $action, $lieu, $type, $public, $partenariat, $participants ) = @_;
+    my $dbh = GetDbh();
+    my $req = "INSERT INTO statdb.stat_action_culturelle (date, action, lieu, type, public, partenariat, participants) VALUES ( ?, ?, ?, ?, ?, ?, ?)";
     my $sth = $dbh->prepare($req);
-    $sth->execute( $date, $action, $lieu, $type, $public, $partenariat, $participants ) ;
+    $sth->execute( $date, $action, $lieu, $type, $public, $partenariat, $participants );
     $sth->finish();
     $dbh->disconnect();
-    AddEsLastAction_culturelle() ;
+    AddEsLastAction_culturelle();
 }
 
 sub list_actions {
-    my $dbh = GetDbh() ;
+    my $dbh = GetDbh();
     my $req = <<SQL;
 SELECT
     id,
