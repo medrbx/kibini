f74e9cff4e8429c4ffe1cdfd0a9cd9a31f4924b6
ragbx <ragbx@ovh.fr>
2019-07-11 15:32:24 +0200

    Suppr tab

--- lib/liste.pm
+++ lib/liste.pm
@@ -22,46 +22,46 @@ sub GetListRows {
 
 sub GetListData {
     my ($params) = @_;
-	
-	my @p = qw ( type loc public wk resbranch );
-	foreach my $p (@p) {
-		$params->{$p} = 'z' if ( !defined $params->{$p} );
-	}
+    
+    my @p = qw ( type loc public wk resbranch );
+    foreach my $p (@p) {
+        $params->{$p} = 'z' if ( !defined $params->{$p} );
+    }
     
     # On forme une clé sur les positions suivantes :
-	#	- 0 : type (d = dispo, t = en traitement, e = expiré, p = perdu, m = mis de côté)
-	#	- 1 : localisation,
-	#	- 2 : semaine,
-	#	- 3 : public,
-	#	- 4 : site de réservation
-	
-	my $key = $params->{type} . $params->{loc} . $params->{public} . $params->{wk} . $params->{resbranch};
+    #    - 0 : type (d = dispo, t = en traitement, e = expiré, p = perdu, m = mis de côté)
+    #    - 1 : localisation,
+    #    - 2 : semaine,
+    #    - 3 : public,
+    #    - 4 : site de réservation
+    
+    my $key = $params->{type} . $params->{loc} . $params->{public} . $params->{wk} . $params->{resbranch};
     
     my %titre = (
         "d0azz" => "Réservations sur documents disponibles, public, RDC",
-		"d0pzz" => "Réservations sur documents disponibles, personnel, RDC",
+        "d0pzz" => "Réservations sur documents disponibles, personnel, RDC",
         "d1azz" => "Réservations sur documents disponibles, public, 1er étage",
-        "d1pzz" => "Réservations sur documents disponibles, personnel, 1er étage",		
+        "d1pzz" => "Réservations sur documents disponibles, personnel, 1er étage",        
         "d2azz" => "Réservations sur documents disponibles, public, 2e étage",
         "d2pzz" => "Réservations sur documents disponibles, personnel, 2e étage",
         "d3azz" => "Réservations sur documents disponibles, public, 3e étage",
         "d3pzz" => "Réservations sur documents disponibles, personnel, 3e étage",
-		"d4azz" => "Réservations sur documents disponibles, public, Zèbre",
+        "d4azz" => "Réservations sur documents disponibles, public, Zèbre",
         "d4pzz" => "Réservations sur documents disponibles, personnel, Zèbre",
         "t0azz" => "Réservations sur documents en traitement, public, RDC",
-		"t0pzz" => "Réservations sur documents en traitement, personnel, RDC",
+        "t0pzz" => "Réservations sur documents en traitement, personnel, RDC",
         "t1azz" => "Réservations sur documents en traitement, public, 1er étage",
-        "t1pzz" => "Réservations sur documents en traitement, personnel, 1er étage",		
+        "t1pzz" => "Réservations sur documents en traitement, personnel, 1er étage",        
         "t2azz" => "Réservations sur documents en traitement, public, 2e étage",
         "t2pzz" => "Réservations sur documents en traitement, personnel, 2e étage",
         "t3azz" => "Réservations sur documents en traitement, public, 3e étage",
         "t3pzz" => "Réservations sur documents en traitement, personnel, 3e étage",
-		"t4azz" => "Réservations sur documents en traitement, public, Zèbre",
+        "t4azz" => "Réservations sur documents en traitement, public, Zèbre",
         "t4pzz" => "Réservations sur documents en traitement, personnel, Zèbre",
         "e0azz" => "Réservations expirées, pour retrait Médiathèque, public",
-		"e0pzz" => "Réservations expirées, pour retrait Médiathèque, personnel",
+        "e0pzz" => "Réservations expirées, pour retrait Médiathèque, personnel",
         "e4zzz" => "Réservations expirées, pour retrait Zèbre",
-		"e00zz" => "Réservations annulées la veille, pour retrait Médiathèque",
+        "e00zz" => "Réservations annulées la veille, pour retrait Médiathèque",
         "mzzzz" => "Réservations mises de côté",
         "p_et0_s1" => "Documents perdus depuis une semaine, RDC",
         "p_et1_s1" => "Documents perdus depuis une semaine, 1er étage",
@@ -79,29 +79,29 @@ sub GetListData {
 
     my %rap = (
         "d0azz" => "128",
-		"d0pzz" => "187",
+        "d0pzz" => "187",
         "d1azz" => "131",
-		"d1pzz" => "188",
+        "d1pzz" => "188",
         "d2azz" => "132",
-		"d2pzz" => "189",
+        "d2pzz" => "189",
         "d3azz" => "133",
-		"d3pzz" => "190",
-		"d4azz" => "170",
-		"d4pzz" => "191",
+        "d3pzz" => "190",
+        "d4azz" => "170",
+        "d4pzz" => "191",
         "t0azz" => "144",
-		"t0pzz" => "192",
+        "t0pzz" => "192",
         "t1azz" => "145",
-		"t1pzz" => "193",
+        "t1pzz" => "193",
         "t2azz" => "146",
-		"t2pzz" => "194",
+        "t2pzz" => "194",
         "t3azz" => "147",
-		"t3pzz" => "195",
-		"t4azz" => "172",
-		"t4pzz" => "196",
+        "t3pzz" => "195",
+        "t4azz" => "172",
+        "t4pzz" => "196",
         "e0azz" => "134",
-		"e0pzz" => "198",
+        "e0pzz" => "198",
         "e4zzz" => "164",
-		"e00zz" => "177",
+        "e00zz" => "177",
         "mzzzz" => "135",
         "p_et0_s1" => "140",
         "p_et1_s1" => "141",
@@ -140,11 +140,11 @@ sub GetListData {
     my $rows = decode_json($rep);
     
     my $data = {
-		titre => $titre,
-		template => $template,
-		rows => $rows
-	};
-	
+        titre => $titre,
+        template => $template,
+        rows => $rows
+    };
+    
     return $data;
 }
 
